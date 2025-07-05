546fa85dda972d49d17535157cb72bd3974d4989
aryan813 <aryanjha813@gmail.com>
2025-07-05 13:45:42 +0530

    Add functions in JS

--- webdev/week1/index.js
+++ webdev/week1/index.js
@@ -7,3 +7,22 @@ console.log(firstName);
 console.log(age);
 console.log(isStudent);
 
+let couple1 = ["john", "jane"]
+console.log(couple1);
+
+// Function
+
+function greet(name) {
+    return "Hello " + firstName;
+}
+
+console.log(greet(firstName));
+
+function canVote(age) {
+    if(age > 18)
+        return true;
+    
+    return false;
+}
+
+console.log(canVote(22));
