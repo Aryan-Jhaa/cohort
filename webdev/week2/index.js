907ce8ab837ff9882c8a80581a458a2afc9c2f91
aryan813 <aryanjha813@gmail.com>
2025-07-07 04:22:05 +0530

    Edit week 2

--- webdev/week2/index.js
+++ webdev/week2/index.js
@@ -3,5 +3,7 @@ const fs = require("fs");
 
 // make sure youre in the same directory as the file you have to read.
 
-const contents = fs.readFileSync("a.txt", "utf-8"); // (file name. encoding)
-console.log(contents);
+const content1 = fs.readFileSync("a.txt", "utf-8"); // (file name. encoding)
+const content2 = fs.readFileSync("b.txt", "utf-8"); // (file name. encoding)
+console.log(content1);
+console.log(content2);
