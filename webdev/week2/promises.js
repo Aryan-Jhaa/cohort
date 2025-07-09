ef01b3bbc4ebfa3e56b1711001c1e7d16544e790
aryan813 <aryanjha813@gmail.com>
2025-07-09 20:11:55 +0530

    Add to promises

--- webdev/week2/promises.js
+++ webdev/week2/promises.js
@@ -4,10 +4,32 @@ function setTimeoutPromisified(ms) {
 }
 
 function callback() {
-    console.log("3 seconds have passed");
+    console.log("3 secs have passed");
     
 }
 
 setTimeoutPromisified(3000).then(callback); // Promisified approach
 
 setTimeout(callback, 3000); // callback approach
+
+
+
+
+
+// *Understanding Promise*
+
+function random(resolve) { // resolve is also a function
+    setTimeout(resolve, 5000); // resolve is called in 5 secs and thats when callback will be called.
+}
+
+let p = new Promise(random); // eventually will return something
+
+
+// after we eventually get the return value:
+
+function callback1() {
+    console.log("Promise completed after 5 secs");
+    
+}
+
+p.then(callback1);
