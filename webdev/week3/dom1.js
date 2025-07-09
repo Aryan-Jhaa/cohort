f40cd5398f9f867c5739f8b5ee1b23c65ce0ed43
aryan813 <aryanjha813@gmail.com>
2025-07-10 04:33:47 +0530

    Add DOM 2

--- webdev/week3/dom1.js
+++ webdev/week3/dom1.js
@@ -1,5 +1,7 @@
+// Updating DOM using JS 
+
 function callback() {
-    document.querySelectorAll("h4")[1].innerHTML = "Changed after 3 secs"
+    document.querySelectorAll("h4")[1].innerHTML = "Changed after 5 secs"
 }
 
-setTimeout(callback, 3000); // calls the callback function every 1 sec.
+setTimeout(callback, 5000); // calls the callback function every 1 sec.
