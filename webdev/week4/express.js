bea9349b072fc8adf09290ce365f15fe1ce15454
aryan813 <aryanjha813@gmail.com>
2025-07-13 07:34:12 +0530

    Add express

--- webdev/week4/express.js
+++ webdev/week4/express.js
@@ -6,5 +6,16 @@ const app = express();
 
 // Define route handlers
 app.get('/', function(req, res){
-    res.send("Hello World")
+    res.send("Hello Week 4");
 });
+
+app.get('/hello', function(req, res){
+    res.send("Hello Week 4 from hello endpoint");
+});
+
+app.post('/', function(req, res){
+    res.send("Hello Week 4 from POST request");
+});
+
+// Specify PORT
+app.listen(3000);
