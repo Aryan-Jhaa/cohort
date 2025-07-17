852b8368bcc0db011918a6d45a0dbe44f0388343
aryan813 <aryanjha813@gmail.com>
2025-07-17 05:56:15 +0530

    Add dynamic route

--- webdev/week5/index.js
+++ webdev/week5/index.js
@@ -11,9 +11,9 @@ app.get("/sum", function(req, res){
     })
 } )
 
-app.get("/multiply", function(req, res){
-    const a = parseInt(req.query.a);
-    const b = parseInt(req.query.b);
+app.get("/multiply/:a/:b", function(req, res){
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
 
     res.json({
         answer: a*b
