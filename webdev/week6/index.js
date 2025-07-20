2fbe5eee27e9ab3247ac1798b68897f147b5f61e
aryan813 <aryanjha813@gmail.com>
2025-07-20 21:49:34 +0530

    Token auth

--- webdev/week6/index.js
+++ webdev/week6/index.js
@@ -49,4 +49,29 @@ if(userFound){
 console.log(users);
 });
 
+// creating an authenticated endpoint: 
+app.get("/me", function(req, res){
+    const token = req.headers.token;
+    let foundUser = null;
+
+    for(let i=0; i<users.length; i++){
+        if(users[i].token == token){
+            foundUser = users[i];
+        }
+    }
+
+    if(foundUser){
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+
+        })
+    }
+    else{
+        res.json({
+            message: "invalid token"
+        })
+    }
+})
+    
 app.listen(3000);
